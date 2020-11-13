b684fe87ee7205ce8404668750fe0888dc34433c
isrt09 <mazedur@gmail.com>
2020-11-13 12:59:54 +0600

    Database connection setup done

---
+++ db/db_connect.php
@@ -0,0 +1,12 @@
+<?php 
+	$server_name = 'localhost';
+	$user_name   = 'root';
+	$password    =  '';
+	$db_name     = 'blog';
+	$con 		 = mysqli_connect($server_name,$user_name,$password,$db_name);
+	if($con){
+		echo "Connected Successfully!!";
+	}else{
+		echo "Connected Failed!!";
+	}
+ ?>
