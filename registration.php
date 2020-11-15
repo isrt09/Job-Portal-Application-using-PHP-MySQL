d1e97e5c87c83a48703866eace688f7db27c11f1
isrt09 <mazedur@gmail.com>
2020-11-15 08:25:06 +0600

    Registration Form Validation added

--- registration.php
+++ registration.php
@@ -1,37 +1,89 @@
 <?php 
+   include('db/db_connect.php');
    include_once('header.php');
+   
+   $first_name_error 	= '';	
+   $middle_name_error 	= '';	
+   $last_name_error 	   = '';	
+   $mobile_error 		   = '';	
+   $email_error 		   = '';	
+   $password_error 		= '';	
+   $confirm_pass_error  = '';
+
+   if(isset($_POST['save'])){
+      extract($_POST);
+      if(!$first_name){
+         $first_name_error = 'First Name Required';
+      }
+      if(!$middle_name){
+         $middle_name_error = 'Middle Name Required';
+      }
+      if(!$last_name){
+         $last_name_error = 'Last Name Required';
+      }
+      if(!$email){
+         $email_error = 'Email Required';
+      }
+      if(!$mobile){
+         $mobile_error = 'Mobile Required';
+      }
+      if(!$password){
+         $password_error = 'Password Required';
+      }
+      if(!$confirm_password){
+         $confirm_pass_error = 'Confirm Password Required';
+      }
+
+      if($first_name && $middle_name && $last_name && $email && $mobile && $password && $confirm_password){
+         $sql = "INSERT INTO tbl_users(first_name,middle_name,last_name,mobile,email,password) VALUES('$first_name','$middle_name','$last_name','$mobile','$email','$password')";
+      
+      $result = mysqli_query($con, $sql);
+         if($result){
+            echo "Data Saved Successfully!";
+         }else{
+            echo "Failed Data Saved into Database";
+         }
+      }      
+   }         
 ?>
 <div class="container">
-   <form action="registration_save.php" method="post">
+   <form action="registration.php" method="post">
       <div class="form-group">
          <label for="first_name">First Name</label>
-         <input type="text" name="first_name" class="form-control" id="first_name">
+         <input type="text" name="first_name" class="form-control" id="first_name" >
+         <span style="color: red;"><?php echo $first_name_error; ?></span>
       </div>
        <div class="form-group">
          <label for="middle_name">Middle Name</label>
-         <input type="text" name="middle_name" class="form-control" id="middle_name">
+         <input type="text" name="middle_name" class="form-control" id="middle_name" >
+         <span style="color: red;"><?php echo $middle_name_error; ?></span>
       </div>
        <div class="form-group">
          <label for="last_name">Last Name</label>
-         <input type="text" name="last_name" class="form-control" id="last_name">
+         <input type="text" name="last_name" class="form-control" id="last_name" >
+         <span style="color: red;"><?php echo $last_name_error; ?></span>
       </div>
        <div class="form-group">
          <label for="mobile">Mobile</label>
-         <input type="text" name="mobile" class="form-control" id="mobile">
+         <input type="text" name="mobile" class="form-control" id="mobile" >
+         <span style="color: red;"><?php echo $mobile_error; ?></span>
       </div>
        <div class="form-group">
          <label for="email">Email</label>
-         <input type="email" name="email" class="form-control" id="email">
+         <input type="email" name="email" class="form-control" id="email" >
+          <span style="color: red;"><?php echo $email_error; ?></span>
       </div>
       <div class="form-group">
          <label for="pwd">Password:</label>
-         <input type="password" class="form-control" id="pwd" name="password">
+         <input type="password" class="form-control" id="pwd" name="password" >
+          <span style="color: red;"><?php echo $password_error; ?></span>
       </div>
       <div class="form-group">
          <label for="confirm_password">Confirm Password:</label>
-         <input type="password" class="form-control" name="confirm_password" id="confirm_password">
+         <input type="password" class="form-control" name="confirm_password" id="confirm_password" >
+         <span style="color: red;"><?php echo $confirm_pass_error; ?></span>
       </div>     
-      <button type="submit" class="btn btn-info mb-5">Submit</button>
+      <button type="submit" class="btn btn-info mb-5" name="save">Submit</button>
    </form>
 </div>
 <?php 
